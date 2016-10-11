727224efce70469c9a04f2187aecdfd99675a4fc
Zequez <zequez@gmail.com>
2016-10-11 19:57:09 -0300

    Store options in localStorage if available

--- src/stores/reducers/optionsReducer.js
+++ src/stores/reducers/optionsReducer.js
@@ -1,9 +1,25 @@
 import { u } from 'lib/utils'
 
-const initialState = {
+function load (initialState) {
+  if (typeof localStorage === 'undefined') return initialState
+  let options
+  try {
+    options = JSON.parse(localStorage.options)
+  } catch (e) {
+    return initialState
+  }
+
+  return {...initialState, ...options}
+}
+
+function save (options) {
+  if (localStorage) localStorage.options = JSON.stringify(options)
+}
+
+const initialState = load({
   batchSize: 20,
   sFilterAdvancedMode: false
-}
+})
 
 export const OPTIONS_SET_ATTR = 'OPTIONS_SET_ATTR'
 
@@ -13,6 +29,7 @@ export function reducer (state = initialState, action) {
   switch (action.type) {
     case OPTIONS_SET_ATTR:
       state = u(state, {[action.attr]: { $set: action.value }})
+      save(state)
       break
   }
 
