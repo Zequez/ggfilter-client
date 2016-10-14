84d6127b17f6dd08abdbe94f8d0bfe67df1e6892
Zequez <zequez@gmail.com>
2016-10-13 21:57:57 -0300

    Re-enable routes, even though we still need to refactor them somehow

--- src/sources/stateRoutes.js
+++ src/sources/stateRoutes.js
@@ -1,9 +1,13 @@
 import StateRouter from 'lib/StateRouter/StateRouter'
 import { encode } from 'lib/b64FilterGenerator'
 
-// import { setFilterFromB64 } from 'src/FilterApp'
 import { MODES, setMode } from 'stores/reducers/uiReducer'
-// import { getFromSid, getFromOfficialSlug } from 'stores/reducers/sFilterReducer'
+
+// TODO: Somehow, don't do this, because we're looking inside FilterApp
+// But if we expose this on src/FilterApp, we get a circular reference
+// Figure it out.
+import { setFilterFromB64 } from 'src/FilterApp/filter/reducer'
+import { getFromSid, getFromOfficialSlug } from 'src/FilterApp/sfilter/reducer'
 
 export default new StateRouter({
   sysreq:
@@ -19,28 +23,28 @@ export default new StateRouter({
   myFilters:
     ['/my-filters', {ui: {mode: MODES.myFilters}}, setMode(MODES.myFilters)],
 
-  // filterB64: [
-  //   '/b/:filterB64', {
-  //     ui: {mode: MODES.filter},
-  //     sfilter: {dirty: true}
-  //   },
-  //   [setMode(MODES.filter), setFilterFromB64],
-  //   (state) => ({filterB64: encode(state.filter)})
-  // ],
-  // filterOfficial: [
-  //   '/:officialSlug', {
-  //     ui: {mode: MODES.filter},
-  //     sfilter: {data: {officialSlug: ':officialSlug'}}
-  //   },
-  //   [setMode(MODES.filter), getFromOfficialSlug]
-  // ],
-  // filterSid: [
-  //   '/f/:filterSid', {
-  //     ui: {mode: MODES.filter},
-  //     sfilter: {data: {sid: ':filterSid'}}
-  //   },
-  //   [setMode(MODES.filter), getFromSid]
-  // ],
+  filterB64: [
+    '/b/:filterB64', {
+      ui: {mode: MODES.filter},
+      sfilter: {dirty: true}
+    },
+    [setMode(MODES.filter), setFilterFromB64],
+    (state) => ({filterB64: encode(state.filter)})
+  ],
+  filterOfficial: [
+    '/:officialSlug', {
+      ui: {mode: MODES.filter},
+      sfilter: {data: {officialSlug: ':officialSlug'}}
+    },
+    [setMode(MODES.filter), getFromOfficialSlug]
+  ],
+  filterSid: [
+    '/f/:filterSid', {
+      ui: {mode: MODES.filter},
+      sfilter: {data: {sid: ':filterSid'}}
+    },
+    [setMode(MODES.filter), getFromSid]
+  ],
   filter:
     ['/', {ui: {mode: MODES.filter}}, setMode(MODES.filter)]
 })
