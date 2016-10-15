0f939c97ade39522908abfb57bd0cf78914e88f0
Zequez <zequez@gmail.com>
2016-10-15 19:53:41 -0300

    Turned static pages into stateless components

--- src/app/components/static/Feedback.jsx
+++ src/app/components/static/Feedback.jsx
@@ -1,17 +1,13 @@
-import React, { PropTypes as t, Component } from 'react'
+import React from 'react'
 
-export default class Feedback extends Component {
-  render () {
-    return (
-      <div className='feedback content-box'>
-        <h2>Feedback</h2>
-        <p>Hey, I hope you like the web app.</p>
-        <p>
-          You can send me feedback directly by <a href='https://www.reddit.com/message/compose/?to=Zequez'>
-            PM on Reddit to /u/Zequez
-          </a>
-        </p>
-      </div>
-    )
-  }
-}
+export default () => (
+  <div className='feedback content-box'>
+    <h2>Feedback</h2>
+    <p>Hey, I hope you like the web app.</p>
+    <p>
+      You can send me feedback directly by <a href='https://www.reddit.com/message/compose/?to=Zequez'>
+        PM on Reddit to /u/Zequez
+      </a>
+    </p>
+  </div>
+)
