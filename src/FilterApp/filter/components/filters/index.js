a4fc009d34c530db5ff4bf65d62d3f5d3778a7f6
Zequez <zequez@gmail.com>
2016-10-16 01:21:20 -0300

    Add index files to the filters, toggles and columns components

---
+++ src/FilterApp/filter/components/filters/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  FancyRange: require('./FancyRangeFilter/FancyRangeFilter'),
+  Tags: require('./TagsFilter/TagsFilter'),
+  Boolean: require('./BooleanFilter'),
+  Null: require('./NullFilter'),
+  Number: require('./NumberFilter'),
+  Range: require('./RangeFilter'),
+  Text: require('./TextFilter')
+}
---
+++ src/FilterApp/games/components/columns/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  Boolean: require('BooleanColumn'),
+  Date: require('DateColumn'),
+  Images: require('ImagesColumn'),
+  Link: require('LinkColumn'),
+  Price: require('PriceColumn'),
+  Ratio: require('RatioColumn'),
+  Raw: require('RawColumn'),
+  SystemReq: require('SystemReqColumn'),
+  Tags: require('TagsColumn'),
+  TimeAgo: require('TimeAgoColumn')
+}
