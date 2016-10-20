00c427521782975500a8c0fcede803e5c4e061cd
Zequez <zequez@gmail.com>
2016-10-20 06:52:58 -0300

    Remove test .only

--- src/shared/lib/SelectorRouter/Route.spec.js
+++ src/shared/lib/SelectorRouter/Route.spec.js
@@ -10,7 +10,7 @@ describe('SelectorRouter/Route', () => {
     })
   })
 
-  describe.only('matchState', () => {
+  describe('matchState', () => {
     let state = {
       mode: 'potato',
       sfilter: {
