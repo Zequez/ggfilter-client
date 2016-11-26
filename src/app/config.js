5019f9f17eb91d8e50acf24ddaeb963a6eaf7a57
Ezequiel Schwartzman <zequez@gmail.com>
2016-11-25 19:52:48 -0500

    Add production switch on the config

--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,8 @@
-export default {
+export default process.env.NODE_ENV === 'production' ? {
+  apiHost: '',
+  origin: 'http://ggfilter.com',
+  bitcoinDonationWallet: '3DJXPqcrZD84wwHMGtJGYYkH21hETuxXWa'
+} : {
   apiHost: 'http://localhost:3000',
   origin: 'http://localhost:8001',
   bitcoinDonationWallet: '3DJXPqcrZD84wwHMGtJGYYkH21hETuxXWa'
