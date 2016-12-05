08980a868f9b8bb43e05c11baf45f360e1a368b7
Ezequiel Schwartzman <zequez@gmail.com>
2016-12-05 14:19:48 -0500

    Small fix of padded decimal numbers on PriceColumn

--- src/FilterApp/components/Table/columns/PriceColumn.jsx
+++ src/FilterApp/components/Table/columns/PriceColumn.jsx
@@ -10,7 +10,7 @@ export default class PriceColumn extends Component {
   elem (val, className) {
     return val != null ? (
       <span className={className}>
-        {val > 0 ? <span><span className={th.__deco}>$</span>{val / 100}</span> : 'Free'}
+        {val > 0 ? <span><span className={th.__deco}>$</span>{(val / 100).toFixed(2)}</span> : 'Free'}
       </span>
     ) : null
   }
