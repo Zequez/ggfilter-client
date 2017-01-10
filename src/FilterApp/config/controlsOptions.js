b6253b96aa94376f3c0eacfa446d8f9f85267c41
Ezequiel Schwartzman <zequez@gmail.com>
2017-01-10 19:08:23 -0300

    Reverse absolute date filter shortcuts order

--- src/FilterApp/config/controlsOptions.js
+++ src/FilterApp/config/controlsOptions.js
@@ -1,91 +1,10 @@
-var yearsAgo = function (years) {
-  return Math.floor(365 * 24 * 60 * 60 * years)
-}
-
-var generateDatesBack = function (years) {
-  let range = [
-    Infinity,
-    0,
-    60 * 60 * 24,
-    60 * 60 * 24 * 7,
-    yearsAgo(1 / 12),
-    yearsAgo(3 / 12),
-    yearsAgo(6 / 12),
-    yearsAgo(1)
-  ]
-  var hash = {
-    range: range,
-    autohook: 0,
-    label: {
-      [range[1]]: 'now',
-      [range[2]]: '24 hours',
-      [range[3]]: '1 week',
-      [range[4]]: '1 month',
-      [range[5]]: '3 months',
-      [range[6]]: '6 months',
-      [range[7]]: '1 year',
-
-      '0->': 'Unreleased',
-      '<-0': 'Released',
-
-      '*-0': 'Last {si}',
-      '<-*': 'Older than {ei}'
-    }
-  }
-
-  for (var i = 2; i <= years; i++) {
-    let ago = yearsAgo(i)
-    hash.range.push(ago)
-    hash.label[ago] = `${i} years`
-  }
-  hash.range.push(null)
-
-  hash.range = hash.range.reverse()
-  return hash
-}
-
-var generateAbsoluteDates = function (startYear, endYear = null) {
-  endYear = endYear || new Date().getFullYear() + 1
-
-  let range = []
-  let namedRanges = {}
-  let invertedNamedRanges = {}
-  for (let i = startYear; i <= endYear; ++i) {
-    let date = Date.parse(`${i}-1-1`) / 1000
-    range.push(date)
-    namedRanges[date] = i
-    invertedNamedRanges[i] = date
-  }
-
-  let rangeInterpolation = (v1, v2, iv1, iv2) => {
-    return iv2 - iv1 < 2 ? iv1 : `[${iv1} to ${iv2 - 1}]`
-  }
-
-  let hash = {
-    range: range,
-    label: {
-      '<-*': null,
-      '*->': null,
-      '*-*': rangeInterpolation,
-      ...namedRanges
-    },
-    byName: invertedNamedRanges
-  }
-
-  return hash
-}
-
-const dd = (strDate) => {
-  return new Date(strDate).valueOf() / 1000
-}
-
 const lastYearsDates = (yearsCount) => {
   let endYear = new Date().getFullYear()
   let startYear = endYear - yearsCount
   let shortcuts = []
-  for (let i = startYear; i <= endYear; ++i) {
+  for (let i = endYear; i >= startYear; --i) {
     let startDate = Date.parse(`${i}-1-1`) / 1000
-    let endDate = Date.parse(`${i+1}-1-1`) / 1000
+    let endDate = Date.parse(`${i + 1}-1-1`) / 1000
     shortcuts.push({
       gt: startDate,
       lt: endDate
@@ -102,9 +21,7 @@ export default {
         fromInput: (value) => value * 100,
         prefix: '$ ',
         focus: 'max'
-      },
-      dateBack: generateDatesBack(15),
-      datesAbsolute: generateAbsoluteDates(1990)
+      }
     }
   },
   shortcuts: {
--- src/FilterApp/config/staticFilters.js
+++ src/FilterApp/config/staticFilters.js
@@ -1,25 +1,25 @@
-import definitions from '../lib/definitions'
+// import definitions from '../lib/definitions'
 
-function releasedYear (year) {
-  return definitions.filters.released_at_absolute.controlOptions.byName[year]
-}
+// function releasedYear (year) {
+//   return definitions.filters.released_at_absolute.controlOptions.byName[year]
+// }
 
 export default {
-  'best-games-of-2014': {
-    title: 'Best games of 2014',
-    filter: {
-      params: {
-        released_at_absolute: {
-          gt: releasedYear(2014),
-          lt: releasedYear(2015)
-        },
-        steam_reviews_count: { gt: 65 },
-        steam_reviews_ratio: { gt: 95 }
-      },
-      sort: {
-        column: 'steam_reviews_ratio',
-        asc: false
-      }
-    }
-  }
+  // 'best-games-of-2014': {
+  //   title: 'Best games of 2014',
+  //   filter: {
+  //     params: {
+  //       released_at_absolute: {
+  //         gt: releasedYear(2014),
+  //         lt: releasedYear(2015)
+  //       },
+  //       steam_reviews_count: { gt: 65 },
+  //       steam_reviews_ratio: { gt: 95 }
+  //     },
+  //     sort: {
+  //       column: 'steam_reviews_ratio',
+  //       asc: false
+  //     }
+  //   }
+  // }
 }
