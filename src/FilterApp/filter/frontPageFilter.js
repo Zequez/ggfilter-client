7ddaa1a14122694b1566e92e1ea18e3ce7b97b18
Ezequiel Schwartzman <zequez@gmail.com>
2017-01-18 00:59:10 -0300

    Updated the front page filter with something useful

--- src/FilterApp/filter/frontPageFilter.js
+++ src/FilterApp/filter/frontPageFilter.js
@@ -2,17 +2,17 @@ import definitions from '../lib/definitions'
 
 export default {
   params: definitions.normalizeParamsOrder({
-    name: { value: 'civ' },
+    name: true,
     tags: true,
-    released_at: true,
+    released_at: {gt: 365 * 24 * 60 * 60, lt: 0},
     lowest_steam_price: true,
     steam_discount: true,
     playtime_median: true,
-    steam_reviews_count: true,
-    steam_reviews_ratio: true
+    steam_reviews_count: { gt: 100 },
+    steam_reviews_ratio: { gt: 90 }
   }),
   sort: {
-    filter: 'name',
-    asc: true
+    filter: 'playtime_median',
+    asc: false
   }
 }
