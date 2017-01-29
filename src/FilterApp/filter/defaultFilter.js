6d37e9e3d0ea93c7aa82743dc13cd27a200daf91
Ezequiel Schwartzman <zequez@gmail.com>
2017-01-29 15:17:47 -0300

    Fixed the default and front page filters

--- src/FilterApp/filter/defaultFilter.js
+++ src/FilterApp/filter/defaultFilter.js
@@ -5,11 +5,11 @@ export default {
     name: true,
     tags: true,
     released_at: true,
-    lowest_steam_price: true,
-    steam_discount: true,
+    lowest_price: true,
+    best_discount: true,
     playtime_median: true,
-    steam_reviews_count: true,
-    steam_reviews_ratio: true
+    ratings_count: true,
+    ratings_ratio: true
   }),
   sort: {
     filter: 'name',
--- src/FilterApp/filter/frontPageFilter.js
+++ src/FilterApp/filter/frontPageFilter.js
@@ -5,11 +5,11 @@ export default {
     name: true,
     tags: true,
     released_at: {gt: 365 * 24 * 60 * 60, lt: 0},
-    lowest_steam_price: true,
-    steam_discount: true,
+    lowest_price: true,
+    best_discount: true,
     playtime_median: true,
-    steam_reviews_count: { gt: 100 },
-    steam_reviews_ratio: { gt: 90 }
+    ratings_count: { gt: 100 },
+    ratings_ratio: { gt: 90 }
   }),
   sort: {
     filter: 'playtime_median',
