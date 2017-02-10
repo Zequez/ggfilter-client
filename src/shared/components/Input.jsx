1cca22207d6abadeebe6202e2952ebd84a871f9c
Ezequiel Schwartzman <zequez@gmail.com>
2017-02-09 22:57:14 -0300

    Improved Input CSS, allows custom themes now

--- src/shared/components/Input.jsx
+++ src/shared/components/Input.jsx
@@ -1,62 +1,67 @@
-import th from './input.sass'
+import theme from './Input.sass'
 import React, { PropTypes as t, Component } from 'react'
 import cx from 'classnames'
 
-export default class Input extends Component {
-  static propTypes = {
-    onChange: t.func,
-    className: t.string,
-    hint: t.string,
-    prefix: t.string,
-    suffix: t.string,
-    label: t.string
-  }
+export const inputWithTheme = (th) => {
+  if (th !== theme) th = { ...theme, ...th }
+  return class Input extends Component {
+    static propTypes = {
+      onChange: t.func,
+      className: t.string,
+      hint: t.string,
+      prefix: t.string,
+      suffix: t.string,
+      label: t.string
+    }
 
-  onChange = (ev) => {
-    if (this.props.onChange) this.props.onChange(ev.target.value)
-  }
+    onChange = (ev) => {
+      if (this.props.onChange) this.props.onChange(ev.target.value)
+    }
 
-  focus () {
-    // We should not need this, it's called after being mounted :S
-    setTimeout(() => { this.refs.input.focus() }, 10)
-  }
-  select () { this.refs.input.select() }
+    focus () {
+      // We should not need this, it's called after being mounted :S
+      setTimeout(() => { this.refs.input.focus() }, 10)
+    }
+    select () { this.refs.input.select() }
 
-  render () {
-    let { className, value, hint, prefix, suffix, label, ...other } = this.props
-    let classes = cx(th.Input, {
-      [className]: !!className,
-      [th.Input_withText]: !!value
-    })
+    render () {
+      let { className, value, hint, prefix, suffix, label, ...other } = this.props
+      let classes = cx(th.Input, {
+        [className]: !!className,
+        [th.Input_withText]: !!value
+      })
 
-    return (
-      <div className={classes}>
-        <div className={th.Input__alignContainer}>
+      return (
+        <div className={classes}>
+          <div className={th.Input__alignContainer}>
 
-          <input
-            type='text'
-            className={th.Input__input}
-            ref='input'
-            {...other}
-            value={value || ''}
-            onChange={this.onChange}/>
-          <div className={th.Input__prefixContainer}>
-            { prefix
-              ? <span className={th.Input__prefix}>{prefix}</span>
+            <input
+              type='text'
+              className={th.Input__input}
+              ref='input'
+              {...other}
+              value={value || ''}
+              onChange={this.onChange}/>
+            <div className={th.Input__prefixContainer}>
+              { prefix
+                ? <span className={th.Input__prefix}>{prefix}</span>
+                : null }
+              { hint
+                ? <div className={th.Input__hint}>{hint}</div>
+                : null }
+              { label
+                ? <div className={th.Input__label}>{label}</div>
+                : null }
+            </div>
+            { suffix
+              ? <div className={th.Input__suffix}>{suffix}</div>
               : null }
-            { hint
-              ? <div className={th.Input__hint}>{hint}</div>
-              : null }
-            { label
-              ? <div className={th.Input__label}>{label}</div>
-              : null }
+            <div className={th.Input__focusLine}></div>
           </div>
-          { suffix
-            ? <div className={th.Input__suffix}>{suffix}</div>
-            : null }
-          <div className={th.Input__focusLine}></div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
+
+export default inputWithTheme(theme)
