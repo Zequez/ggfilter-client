c58d2c59c69170e528b0413d980afdd36ec9da9d
Ezequiel Schwartzman <zequez@gmail.com>
2017-07-16 13:30:19 -0300

    Moved the client-gem into a the parent folder

--- copy_version_to_gem.rb
+++ copy_version_to_gem.rb
@@ -1,6 +1,6 @@
 require 'json'
 
 version = JSON.load(File.read('./package.json'))['version']
-gemspec = File.read('./gem/ggfilter_client.gemspec')
+gemspec = File.read('../client-gem/ggfilter_client.gemspec')
   .sub(/(version = ')([^']+)(')/, "\\1#{version}\\3")
-File.write('./gem/ggfilter_client.gemspec', gemspec)
+File.write('../client-gem/ggfilter_client.gemspec', gemspec)
--- gem/lib/ggfilter_client.rb
+++
@@ -1,6 +0,0 @@
-module GgfilterClient
-  module Rails
-    class Engine < ::Rails::Engine
-    end
-  end
-end
